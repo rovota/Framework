b2ff8f92467706d73c867452cac544cb6b0be8c3
Léandro Tijink <leandro.tijink@gmail.com>
2024-07-15 13:53:31 +0200

    added: various global helper functions

--- src/functions.php
+++ src/functions.php
@@ -5,6 +5,11 @@
  * @license     MIT
  */
 
+use Dflydev\DotAccessData\Data;
+use Rovota\Framework\Kernel\Application;
+use Rovota\Framework\Structures\Map;
+use Rovota\Framework\Structures\Sequence;
+use Rovota\Framework\Support\Arr;
 use Rovota\Framework\Support\Interfaces\Arrayable;
 use Rovota\Framework\Support\Str;
 use Rovota\Framework\Support\Text;
@@ -57,11 +62,42 @@ if (!function_exists('e')) {
 // -----------------
 
 // -----------------
+// Comparisons
+
+if (!function_exists('float_equals')) {
+	/**
+	 * Determines whether two floating values are equal, accounting for rounding precision.
+	 */
+	function float_equals(float $first, float $second, int $precision = Application::DEFAULT_FLOAT_PRECISION): bool
+	{
+		return round($first, $precision) === round($second, $precision);
+	}
+}
+
+// -----------------
+// Structures
+
+if (!function_exists('as_map')) {
+	function as_map(mixed $items = []): Map
+	{
+		return new Map($items);
+	}
+}
+
+if (!function_exists('as_sequence')) {
+	function as_sequence(mixed $items = []): Sequence
+	{
+		return new Sequence($items);
+	}
+}
 
 // -----------------
 // Internal
 
 if (!function_exists('source')) {
+	/**
+	 * Returns a complete path to a given file or directory in the framework folder, where `bootloader.php` is located.
+	 */
 	function source(string $path = ''): string
 	{
 		return local($path, dirname(__FILE__));
@@ -69,10 +105,27 @@ if (!function_exists('source')) {
 }
 
 if (!function_exists('local')) {
+	/**
+	 * Returns a complete path to a given file or directory in the application folder, where `app.php` is located.
+	 */
 	function local(string $path = '', string|null $base = null): string
 	{
 		$base = $base ?? (defined('BASE_PATH') ? BASE_PATH : dirname(__FILE__));
 		return strlen($path) > 0 ? $base.'/'.ltrim($path, '/') : $base;
+	}
+}
+
+if (!function_exists('convert_to_array')) {
+	function convert_to_array(mixed $value): array
+	{
+		return match(true) {
+			$value === null => [],
+			is_array($value) => $value,
+			$value instanceof Arrayable => $value->toArray(),
+			$value instanceof JsonSerializable => convert_to_array($value->jsonSerialize()),
+			$value instanceof Data => $value->export(),
+			default => [$value],
+		};
 	}
 }
 
@@ -136,5 +189,12 @@ if (!function_exists('data_get')) {
 		}
 
 		return $target;
+	}
+}
+
+if (!function_exists('json_encode_clean')) {
+	function json_encode_clean(mixed $value, int $depth = 512): false|string
+	{
+		return json_encode($value, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK, $depth);
 	}
 }
