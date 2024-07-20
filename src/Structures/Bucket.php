9d75fb255af089924b2ecbd5d493536d0247d0c4
Léandro Tijink <leandro.tijink@gmail.com>
2024-07-20 21:20:38 +0200

    refactor: split Bucket creating SimpleBucket

--- src/Structures/Bucket.php
+++ src/Structures/Bucket.php
@@ -7,74 +7,17 @@
 
 namespace Rovota\Framework\Structures;
 
-use ArrayAccess;
-use ArrayIterator;
 use Closure;
-use Countable;
 use Dflydev\DotAccessData\Data;
-use Dflydev\DotAccessData\DataInterface;
-use IteratorAggregate;
-use JsonSerializable;
 use Rovota\Framework\Kernel\Application;
 use Rovota\Framework\Support\Arr;
 use Rovota\Framework\Support\Interfaces\Arrayable;
 use Rovota\Framework\Support\Internal;
 use Rovota\Framework\Support\Math;
-use Rovota\Framework\Support\Traits\Conditionable;
-use Rovota\Framework\Support\Traits\Macroable;
-use Rovota\Framework\Support\Traits\TypeAccessors;
 use Stringable;
-use Traversable;
 
-class Bucket implements ArrayAccess, IteratorAggregate, Countable, Arrayable, JsonSerializable
+class Bucket extends SimpleBucket
 {
-	use TypeAccessors, Conditionable, Macroable;
-
-	protected Data $items;
-
-	// -----------------
-
-	public function __construct(mixed $items = [])
-	{
-		$this->items = new Data(convert_to_array($items));
-	}
-
-	public function import(mixed $data, bool $preserve = false): static
-	{
-		$mode = $preserve ? DataInterface::PRESERVE : DataInterface::MERGE;
-		$this->items->import(convert_to_array($data), $mode);
-		return $this;
-	}
-
-	// -----------------
-
-	public function keys(): Sequence
-	{
-		return new Sequence(array_keys($this->toArray()));
-	}
-
-	public function values(): Sequence
-	{
-		return new Sequence(array_values($this->toArray()));
-	}
-
-	// -----------------
-
-	public function flush(): static
-	{
-		$this->items = new Data();
-		return $this;
-	}
-
-	public function copy(): static
-	{
-		return clone $this;
-	}
-
-	public function count(mixed $key = null): int
-	{
-		return count($key !== null ? ($this->get($key) ?? []) : $this->toArray());
-	}
 
 	public function countBy(callable|null $callback = null): static
 	{
@@ -89,13 +32,6 @@ class Bucket implements ArrayAccess, IteratorAggregate, Countable, Arrayable, Js
 		}
 
 		return $counted;
-	}
-
-	// -----------------
-
-	public function isEmpty(): bool
-	{
-		return empty($this->toArray());
 	}
 
 	// -----------------
@@ -132,65 +68,6 @@ class Bucket implements ArrayAccess, IteratorAggregate, Countable, Arrayable, Js
 
 	// -----------------
 
-	public function has(mixed $key): bool
-	{
-		foreach (is_array($key) ? $key : [$key] as $item) {
-			if ($this->offsetExists($item) === false) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	public function missing(mixed $key): bool
-	{
-		foreach (is_array($key) ? $key : [$key] as $item) {
-			if ($this->offsetExists($item) === true) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	public function pull(mixed $key, mixed $default = null): mixed
-	{
-		$value = $this->offsetGet($key) ?? $default;
-		$this->offsetUnset($key);
-		return $value;
-	}
-
-	public function get(mixed $key, mixed $default = null): mixed
-	{
-		if (is_object($key)) {
-			$key = spl_object_hash($key);
-		}
-		return $this->items->get($key, ($default instanceof Closure ? $default() : $default));
-	}
-
-	public function remove(mixed $key): static
-	{
-		foreach (is_array($key) ? $key : [$key] as $item) {
-			$this->offsetUnset($item);
-		}
-		return $this;
-	}
-
-	public function find(mixed $value): int|null
-	{
-		return Arr::search($this->toArray(), $value);
-	}
-
-	public function set(mixed $key, mixed $value = null): void
-	{
-		if (is_array($key)) {
-			foreach ($key as $offset => $item) {
-				$this->offsetSet($offset, $item);
-			}
-		} else {
-			$this->offsetSet($key, $value);
-		}
-	}
-
 	public function random(int $amount = 1): static
 	{
 		$result = Arr::random($this->toArray(), $amount);
@@ -199,40 +76,12 @@ class Bucket implements ArrayAccess, IteratorAggregate, Countable, Arrayable, Js
 
 	// -----------------
 
-	public function append(mixed $value): static
-	{
-		foreach (is_array($value) ? $value : [$value] as $item) {
-			$this->offsetSet(null, $item);
-		}
-		return $this;
-	}
-
-	public function prepend(mixed $value): static
-	{
-		$original = $this->toArray();
-		array_unshift($original, $value);
-		$this->items = new Data($original);
-		return $this;
-	}
-
 	public function concat(mixed $data): static
 	{
 		foreach (convert_to_array($data) as $item) {
 			$this->append($item);
 		}
 		return $this;
-	}
-
-	// -----------------
-
-	public function first(callable|null $callback = null, mixed $default = null): mixed
-	{
-		return Arr::first($this->toArray(), $callback, $default);
-	}
-
-	public function last(callable|null $callback = null, mixed $default = null): mixed
-	{
-		return Arr::last($this->toArray(), $callback, $default);
 	}
 
 	// -----------------
@@ -292,36 +141,6 @@ class Bucket implements ArrayAccess, IteratorAggregate, Countable, Arrayable, Js
 		}
 
 		return $duplicates;
-	}
-
-	// -----------------
-
-	/**
-	 * Only returns items that match a given truth test.
-	 */
-	public function filter(callable $callback): static
-	{
-		return new static(Arr::filter($this->toArray(), $callback));
-	}
-
-	/**
-	 * Returns all items, except for those with the key(s) given.
-	 */
-	public function except(array $keys): static
-	{
-		return $this->copy()->remove($keys);
-	}
-
-	/**
-	 * Returns only the items which are specified by key.
-	 */
-	public function only(array $keys): static
-	{
-		$bucket = new static();
-		foreach ($keys as $key) {
-			$bucket->set($key, $this->items->get($key));
-		}
-		return $bucket;
 	}
 
 	// -----------------
@@ -643,20 +462,6 @@ class Bucket implements ArrayAccess, IteratorAggregate, Countable, Arrayable, Js
 
 	// -----------------
 
-	public function increment(mixed $key, int $step = 1): static
-	{
-		$this->set($key, (int) $this->get($key, 0) + abs($step));
-		return $this;
-	}
-
-	public function decrement(mixed $key, int $step = 1): static
-	{
-		$this->set($key, (int) $this->get($key, 0) - abs($step));
-		return $this;
-	}
-
-	// -----------------
-
 	public function skip(int $count): static
 	{
 		if ($this->count() <= $count) {
@@ -749,112 +554,6 @@ class Bucket implements ArrayAccess, IteratorAggregate, Countable, Arrayable, Js
 	public function diffKeys(mixed $items): static
 	{
 		return new static(array_diff_key($this->toArray(), convert_to_array($items)));
-	}
-
-	// -----------------
-
-	// -----------------
-
-	// -----------------
-
-	// -----------------
-
-	// -----------------
-
-	// -----------------
-
-	// -----------------
-
-	// -----------------
-
-	// -----------------
-
-	public function toArray(): array
-	{
-		return $this->items->export();
-	}
-
-	public function toJson(): string
-	{
-		return json_encode_clean($this->items->export());
-	}
-
-	public function toMap(): Map
-	{
-		return new Map($this->items->export());
-	}
-
-	public function toSequence(): Sequence
-	{
-		return new Sequence($this->items->export());
-	}
-
-	// -----------------
-
-	/**
-	 * @internal
-	 */
-	public function jsonSerialize(): array
-	{
-		return $this->toArray();
-	}
-
-	/**
-	 * @internal
-	 */
-	public function getIterator(): Traversable
-	{
-		return new ArrayIterator($this->items->export());
-	}
-
-	/**
-	 * @internal
-	 */
-	public function offsetExists(mixed $offset): bool
-	{
-		if (is_object($offset)) {
-			$offset = spl_object_hash($offset);
-		}
-		return $this->items->has($offset);
-	}
-
-	/**
-	 * @internal
-	 */
-	public function offsetGet(mixed $offset): mixed
-	{
-		if (is_object($offset)) {
-			$offset = spl_object_hash($offset);
-		}
-		return $this->items->get($offset);
-	}
-
-	/**
-	 * @internal
-	 */
-	public function offsetSet(mixed $offset, mixed $value): void
-	{
-		if ($offset === null) {
-			$items = $this->toArray();
-			$items[] = $value;
-			$this->items = new Data($items);
-		} else {
-			if (is_object($offset)) {
-				$offset = spl_object_hash($offset);
-			}
-			$this->items->set($offset, $value);
-		}
-	}
-
-	/**
-	 * @internal
-	 */
-	public function offsetUnset(mixed $offset): void
-	{
-		if (is_object($offset)) {
-			$offset = spl_object_hash($offset);
-		}
-		$this->items->remove($offset);
 	}
 
 }
---
+++ src/Structures/SimpleBucket.php
@@ -0,0 +1,307 @@
+<?php
+
+/**
+ * @copyright   Léandro Tijink
+ * @license     MIT
+ */
+
+namespace Rovota\Framework\Structures;
+
+use ArrayAccess;
+use ArrayIterator;
+use Closure;
+use Countable;
+use Dflydev\DotAccessData\Data;
+use Dflydev\DotAccessData\DataInterface;
+use IteratorAggregate;
+use JsonSerializable;
+use Rovota\Framework\Support\Arr;
+use Rovota\Framework\Support\Interfaces\Arrayable;
+use Rovota\Framework\Support\Traits\Conditionable;
+use Rovota\Framework\Support\Traits\Macroable;
+use Rovota\Framework\Support\Traits\TypeAccessors;
+use Traversable;
+
+class SimpleBucket implements ArrayAccess, IteratorAggregate, Countable, Arrayable, JsonSerializable
+{
+	use TypeAccessors, Conditionable, Macroable;
+
+	protected Data $items;
+
+	// -----------------
+
+	public function __construct(mixed $items = [])
+	{
+		$this->items = new Data(convert_to_array($items));
+	}
+
+	public function import(mixed $data, bool $preserve = false): static
+	{
+		$mode = $preserve ? DataInterface::PRESERVE : DataInterface::MERGE;
+		$this->items->import(convert_to_array($data), $mode);
+		return $this;
+	}
+
+	// -----------------
+
+	public function keys(): Sequence
+	{
+		return new Sequence(array_keys($this->toArray()));
+	}
+
+	public function values(): Sequence
+	{
+		return new Sequence(array_values($this->toArray()));
+	}
+
+	// -----------------
+
+	public function flush(): static
+	{
+		$this->items = new Data();
+		return $this;
+	}
+
+	public function copy(): static
+	{
+		return clone $this;
+	}
+
+	public function count(mixed $key = null): int
+	{
+		return count($key !== null ? ($this->get($key) ?? []) : $this->toArray());
+	}
+
+	// -----------------
+
+	public function isEmpty(): bool
+	{
+		return empty($this->toArray());
+	}
+
+	// -----------------
+
+	public function has(mixed $key): bool
+	{
+		foreach (is_array($key) ? $key : [$key] as $item) {
+			if ($this->offsetExists($item) === false) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public function missing(mixed $key): bool
+	{
+		foreach (is_array($key) ? $key : [$key] as $item) {
+			if ($this->offsetExists($item) === true) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public function pull(mixed $key, mixed $default = null): mixed
+	{
+		$value = $this->offsetGet($key) ?? $default;
+		$this->offsetUnset($key);
+		return $value;
+	}
+
+	public function get(mixed $key, mixed $default = null): mixed
+	{
+		if (is_object($key)) {
+			$key = spl_object_hash($key);
+		}
+		return $this->items->get($key, ($default instanceof Closure ? $default() : $default));
+	}
+
+	public function remove(mixed $key): static
+	{
+		foreach (is_array($key) ? $key : [$key] as $item) {
+			$this->offsetUnset($item);
+		}
+		return $this;
+	}
+
+	public function find(mixed $value): string|int|null
+	{
+		return Arr::search($this->toArray(), $value);
+	}
+
+	public function set(mixed $key, mixed $value = null): void
+	{
+		if (is_array($key)) {
+			foreach ($key as $offset => $item) {
+				$this->offsetSet($offset, $item);
+			}
+		} else {
+			$this->offsetSet($key, $value);
+		}
+	}
+
+	// -----------------
+
+	public function append(mixed $value): static
+	{
+		foreach (is_array($value) ? $value : [$value] as $item) {
+			$this->offsetSet(null, $item);
+		}
+		return $this;
+	}
+
+	public function prepend(mixed $value): static
+	{
+		$original = $this->toArray();
+		array_unshift($original, $value);
+		$this->items = new Data($original);
+		return $this;
+	}
+
+	// -----------------
+
+	public function first(callable|null $callback = null, mixed $default = null): mixed
+	{
+		return Arr::first($this->toArray(), $callback, $default);
+	}
+
+	public function last(callable|null $callback = null, mixed $default = null): mixed
+	{
+		return Arr::last($this->toArray(), $callback, $default);
+	}
+
+	// -----------------
+
+	/**
+	 * Only returns items that match a given truth test.
+	 */
+	public function filter(callable $callback): static
+	{
+		return new static(Arr::filter($this->toArray(), $callback));
+	}
+
+	/**
+	 * Returns all items, except for those with the key(s) given.
+	 */
+	public function except(array $keys): static
+	{
+		return $this->copy()->remove($keys);
+	}
+
+	/**
+	 * Returns only the items which are specified by key.
+	 */
+	public function only(array $keys): static
+	{
+		$bucket = new static();
+		foreach ($keys as $key) {
+			$bucket->set($key, $this->items->get($key));
+		}
+		return $bucket;
+	}
+
+	// -----------------
+
+	public function increment(mixed $key, int $step = 1): static
+	{
+		$this->set($key, (int) $this->get($key, 0) + abs($step));
+		return $this;
+	}
+
+	public function decrement(mixed $key, int $step = 1): static
+	{
+		$this->set($key, (int) $this->get($key, 0) - abs($step));
+		return $this;
+	}
+
+	// -----------------
+
+	public function toArray(): array
+	{
+		return $this->items->export();
+	}
+
+	public function toJson(): string
+	{
+		return json_encode_clean($this->items->export());
+	}
+
+	public function toMap(): Map
+	{
+		return new Map($this->items->export());
+	}
+
+	public function toSequence(): Sequence
+	{
+		return new Sequence($this->items->export());
+	}
+
+	// -----------------
+
+	/**
+	 * @internal
+	 */
+	public function jsonSerialize(): array
+	{
+		return $this->toArray();
+	}
+
+	/**
+	 * @internal
+	 */
+	public function getIterator(): Traversable
+	{
+		return new ArrayIterator($this->items->export());
+	}
+
+	/**
+	 * @internal
+	 */
+	public function offsetExists(mixed $offset): bool
+	{
+		if (is_object($offset)) {
+			$offset = spl_object_hash($offset);
+		}
+		return $this->items->has($offset);
+	}
+
+	/**
+	 * @internal
+	 */
+	public function offsetGet(mixed $offset): mixed
+	{
+		if (is_object($offset)) {
+			$offset = spl_object_hash($offset);
+		}
+		return $this->items->get($offset);
+	}
+
+	/**
+	 * @internal
+	 */
+	public function offsetSet(mixed $offset, mixed $value): void
+	{
+		if ($offset === null) {
+			$items = $this->toArray();
+			$items[] = $value;
+			$this->items = new Data($items);
+		} else {
+			if (is_object($offset)) {
+				$offset = spl_object_hash($offset);
+			}
+			$this->items->set($offset, $value);
+		}
+	}
+
+	/**
+	 * @internal
+	 */
+	public function offsetUnset(mixed $offset): void
+	{
+		if (is_object($offset)) {
+			$offset = spl_object_hash($offset);
+		}
+		$this->items->remove($offset);
+	}
+
+}
