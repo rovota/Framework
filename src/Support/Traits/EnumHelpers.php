ef6620bbf3c4eb088298cabbbc34328143f8c559
Léandro Tijink <leandro.tijink@gmail.com>
2024-07-21 10:42:43 +0200

    removed: redundant todo comment

--- src/Support/Traits/EnumHelpers.php
+++ src/Support/Traits/EnumHelpers.php
@@ -12,8 +12,6 @@ use Rovota\Framework\Support\Arr;
 trait EnumHelpers
 {
 
-	// TODO: Implement Arr class
-
 	public function isAny(array $items): bool
 	{
 		return Arr::containsAny($items, [$this]);
