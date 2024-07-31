f3221ea2cf657826c3bfd1d5550e9fe31404b342
Léandro Tijink <leandro.tijink@gmail.com>
2024-07-31 22:52:07 +0200

    added: reponse validation methods

--- src/Http/Client/Traits/ResponseValidation.php
+++ src/Http/Client/Traits/ResponseValidation.php
@@ -9,19 +9,86 @@
 
 namespace Rovota\Framework\Http\Client\Traits;
 
+use Rovota\Framework\Http\Enums\StatusCode;
+
 trait ResponseValidation
 {
 
-	// -----------------
+	public function isFailure(): bool
+	{
+		return $this->isServerError() || $this->isClientError();
+	}
 
 	// -----------------
+	// 2xx
+
+	public function isSuccess(): bool
+	{
+		return $this->status()->value >= 200 && $this->status()->value < 300;
+	}
+
+	public function isOk(): bool
+	{
+		return $this->status() === StatusCode::Ok;
+	}
 
 	// -----------------
+	// 3xx
+
+	public function isRedirect(): bool
+	{
+		return $this->status()->value >= 300 && $this->status()->value < 400;
+	}
 
 	// -----------------
+	// 4xx
+
+	public function isClientError(): bool
+	{
+		return $this->status()->value >= 400 && $this->status()->value < 500;
+	}
+
+	public function isUnauthorized(): bool
+	{
+		return $this->status() === StatusCode::Unauthorized;
+	}
+
+	public function isPaymentRequired(): bool
+	{
+		return $this->status() === StatusCode::PaymentRequired;
+	}
+
+	public function isForbidden(): bool
+	{
+		return $this->status() === StatusCode::Forbidden;
+	}
+
+	public function isNotFound(): bool
+	{
+		return $this->status() === StatusCode::NotFound;
+	}
+
+	public function isTooManyRequests(): bool
+	{
+		return $this->status() === StatusCode::TooManyRequests;
+	}
 
 	// -----------------
+	// 5xx
 
-	// -----------------
+	public function isServerError(): bool
+	{
+		return $this->status()->value >= 500;
+	}
+
+	public function isServiceUnavailable(): bool
+	{
+		return $this->status() === StatusCode::ServiceUnavailable;
+	}
+
+	public function isNotImplemented(): bool
+	{
+		return $this->status() === StatusCode::NotImplemented;
+	}
 
 }
