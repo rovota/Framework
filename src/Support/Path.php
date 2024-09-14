83349ce3e870becac5f27e86ce85e6d7d67013a8
Léandro Tijink <leandro.tijink@gmail.com>
2024-09-14 21:56:00 +0200

    fixed: no longer allow empty paths on Path methods

--- src/Support/Path.php
+++ src/Support/Path.php
@@ -36,7 +36,7 @@ final class Path
 	/**
 	 * Returns a complete path to a given file in the framework folder, where `bootloader.php` is located.
 	 */
-	public static function toSourceFile(string $path = ''): string
+	public static function toSourceFile(string $path): string
 	{
 		return self::toProjectFile($path, self::getFrameworkRootPath());
 	}
@@ -44,10 +44,9 @@ final class Path
 	/**
 	 * Returns a complete path to a given file in the project folder, where `app.php` is located.
 	 */
-	public static function toProjectFile(string $path = '', string|null $base = null): string
+	public static function toProjectFile(string $path, string|null $base = null): string
 	{
-		$base = $base ?? self::getProjectRootPath();
-		return strlen($path) > 0 ? $base.'/'.ltrim($path, '/') : $base;
+		return ($base ?? self::getProjectRootPath()).'/'.ltrim($path, '/');
 	}
 
 	// -----------------
