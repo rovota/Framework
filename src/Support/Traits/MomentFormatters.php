47b27ba94b0b503c03c6eb953a455da06ca50c0f
Léandro Tijink <leandro.tijink@gmail.com>
2024-12-27 23:25:04 +0100

    added: initial MomentFormatters class

---
+++ src/Support/Traits/MomentFormatters.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @copyright   Léandro Tijink
+ * @license     MIT
+ *
+ * Inspired by the Laravel/Conditionable trait.
+ */
+
+namespace Rovota\Framework\Support\Traits;
+
+trait MomentFormatters
+{
+
+	public function toEpochString(): string
+	{
+		return $this->format('U');
+	}
+
+	// -----------------
+
+	// -----------------
+
+	// -----------------
+
+	// -----------------
+
+	// -----------------
+
+}
