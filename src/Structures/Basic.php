e01930e15da4530fb385353c2bf437c0e4c7f7e4
Léandro Tijink <leandro.tijink@gmail.com>
2025-01-01 16:03:48 +0100

    added: Basic structure class

---
+++ src/Structures/Basic.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @copyright   Léandro Tijink
+ * @license     MIT
+ */
+
+namespace Rovota\Framework\Structures;
+
+use Rovota\Framework\Support\Interfaces\Arrayable;
+
+abstract class Basic implements Arrayable
+{
+
+	protected array $attributes = [];
+
+	// -----------------
+
+	public function __construct(array $attributes = [])
+	{
+		$this->attributes = $attributes;
+	}
+
+	// -----------------
+
+	public function __get(string $name)
+	{
+		return $this->attributes[$name] ?? null;
+	}
+
+	public function __set(string $name, mixed $value)
+	{
+		$this->attributes[$name] = $value;
+	}
+
+	// -----------------
+
+	public function toArray(): array
+	{
+		return $this->attributes;
+	}
+
+}
