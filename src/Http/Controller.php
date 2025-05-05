1c1fbfb52abd6036033748849272b8b7f5e628be
Léandro Tijink <leandro.tijink@gmail.com>
2025-05-05 13:27:46 +0200

    added: configuration method approach to Controller

--- src/Http/Controller.php
+++ src/Http/Controller.php
@@ -12,11 +12,23 @@ use BadMethodCallException;
 class Controller
 {
 
+	public function __construct()
+	{
+		$this->configuration();
+	}
+
 	public function __call($method, $parameters)
 	{
 		throw new BadMethodCallException(
 			sprintf('Method %s::%s does not exist.', static::class, $method)
 		);
+	}
+
+	// -----------------
+
+	protected function configuration(): void
+	{
+
 	}
 
 }
