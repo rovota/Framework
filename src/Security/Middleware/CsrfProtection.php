fbeb3d37c27505e99f84971f54b0ed1951cefcd0
Léandro Tijink <leandro.tijink@gmail.com>
2025-05-17 09:08:39 +0200

    changed: take csrf token from both get and post data

--- src/Security/Middleware/CsrfProtection.php
+++ src/Security/Middleware/CsrfProtection.php
@@ -16,14 +16,14 @@ use Rovota\Framework\Security\CsrfManager;
 class CsrfProtection
 {
 	/**
-	 * Reject requests that are using the POST method, but do not specify a CSRF token.
+	 * Reject requests that are using the POST method but do not specify a CSRF token.
 	 */
 	public function handle(RequestObject $request): void
 	{
 		$token_name = CsrfManager::getTokenName();
 		$token_value = CsrfManager::getToken();
 
-		if ($request->isPost() && $request->post->get($token_name) !== $token_value) {
+		if ($request->isPost() && $request->get($token_name) !== $token_value) {
 			echo response(StatusCode::Forbidden); exit;
 		}
 
