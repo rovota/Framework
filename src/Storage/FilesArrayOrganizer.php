58cc9e45925283cda8016984c272a8432762a4fe
Léandro Tijink <leandro.tijink@gmail.com>
2025-08-15 22:59:23 +0200

    fixed: prevent failed uploads from appearing in uploaded files data

--- src/Storage/FilesArrayOrganizer.php
+++ src/Storage/FilesArrayOrganizer.php
@@ -16,70 +16,74 @@ use Rovota\Framework\Http\Request\UploadedFile;
 final class FilesArrayOrganizer
 {
 
-   public static function organize(array $filesArray): array
-   {
-      $output = [];
-      $flattenedFilesArray = [];
+	public static function organize(array $filesArray): array
+	{
+		$output = [];
+		$flattenedFilesArray = [];
 
-      foreach ($filesArray as $top_level_name => $attributes) {
-         $output[$top_level_name] = [];
+		foreach ($filesArray as $top_level_name => $attributes) {
+			$output[$top_level_name] = [];
 
-         foreach ($attributes as $attribute_name => $attribute_values) {
-            FilesArrayOrganizer::inner(
-               $output[$top_level_name],
-               $attribute_name,
-               $attribute_values,
-               $flattenedFilesArray
-            );
-         }
-      }
+			foreach ($attributes as $attribute_name => $attribute_values) {
+				FilesArrayOrganizer::inner(
+					$output[$top_level_name],
+					$attribute_name,
+					$attribute_values,
+					$flattenedFilesArray
+				);
+			}
+		}
 
-      foreach ($flattenedFilesArray as & $file) {
-         $file = new UploadedFile($file['name'], $file['tmp_name'], $file['error']);
-      }
+		foreach ($output as $name => $attributes) {
+			if ($attributes['error'] === UPLOAD_ERR_OK) {
+				$output[$name] = new UploadedFile($attributes['name'], $attributes['tmp_name'], $attributes['error']);
+			} else {
+				unset($output[$name]);
+			}
+		}
 
-      return $output;
-   }
+		return $output;
+	}
 
 
-   private static function inner(array &$root_element, string $attribute_name, mixed $value, array &$files = [], array $path = []): void
-   {
-      // If $value is not an array then we've arrived at the attribute value
-      if (!is_array($value)) {
-         $last_key = $attribute_name;
-         $stage = &$root_element;
+	private static function inner(array &$root_element, string $attribute_name, mixed $value, array &$files = [], array $path = []): void
+	{
+		// If $value is not an array, then we've arrived at the attribute value
+		if (!is_array($value)) {
+			$last_key = $attribute_name;
+			$stage = &$root_element;
 
-         // With each element of $path, go one stage deeper into $root_element
-         foreach ($path as $path_segment) {
-            // If the stage doesn't exist yet, create it
-            if (!isset($stage[$path_segment])) {
-               $stage[$path_segment] = [];
-            }
-            $stage = &$stage[$path_segment];
-         }
+			// With each element of $path, go one stage deeper into $root_element
+			foreach ($path as $path_segment) {
+				// If the stage doesn't exist yet, create it
+				if (!isset($stage[$path_segment])) {
+					$stage[$path_segment] = [];
+				}
+				$stage = &$stage[$path_segment];
+			}
 
-         // Add the final stage to the $files array
-         if (!in_array($stage, $files)) {
-            $files[] = &$stage;
-         }
-         $stage[$last_key] = $value;
+			// Add the final stage to the $files array
+			if (!in_array($stage, $files)) {
+				$files[] = &$stage;
+			}
+			$stage[$last_key] = $value;
 
-         return;
-      }
+			return;
+		}
 
-      // If $value is an array, recurse into it, building up $infix_path
-      foreach ($value as $child_field_index => $child_field_values) {
-         // Create new array one path segment longer, without mutating $path
-         $infix_path = array_merge($path, [$child_field_index]);
+		// If $value is an array, recurse into it, building up $infix_path
+		foreach ($value as $child_field_index => $child_field_values) {
+			// Create a new array one path segment longer, without mutating $path
+			$infix_path = array_merge($path, [$child_field_index]);
 
-         FilesArrayOrganizer::inner(
-            $root_element,
-            $attribute_name,
-            $child_field_values,
-            $files,
-            $infix_path
-         );
-      }
-   }
+			FilesArrayOrganizer::inner(
+				$root_element,
+				$attribute_name,
+				$child_field_values,
+				$files,
+				$infix_path
+			);
+		}
+	}
 
 }
